f144947e63fc22415bde46b093e8c19fe521d547
cmattox4846 <cmattox4846@gmail.com>
2021-10-21 18:03:11 -0400

    have create,index,detail html

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
 
 # Create your views here.
@@ -18,12 +18,21 @@ def detail(request,hero_id):
     }
     return render(request,'superheroes/detail.html', context)
 
-def create(request,hero_id):
-    create_hero = Superhero.objects.
-    context ={
-        'create_hero':create_hero
-    }
-    return render(request,'superheroes/create.html', context)
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catch_phrase = request.POST.get('catchphrase')
+
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability = primary, secondary_ability= secondary, catch_phrase = catch_phrase)
+        new_hero.save()
+
+        return HttpResponseRedirect(reverse('superheroes:index'))
+
+    else:
+        return render(request,'superheroes/create.html')
 
 def edit(request,hero_id):
     edit_hero = Superhero.objects.get(pk = hero_id)
